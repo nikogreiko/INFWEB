5a1a686126d18afc31c264da8e4e615b45dd3981
nikogreiko <programmingpapas@gmail.com>
2014-03-29 12:36:31 -0400

    WeB

---
+++ README.txt
@@ -0,0 +1,5 @@
+﻿Le point d'entrée principal du site est la page index.html
+La page d'accueil des étudiants est etudiant/index.html
+La page d'accueil des démonstrateurs est demo/index.html
+La page d'accueil du professeur est prof/index.html
+Le codage pour toutes les pages est UTF-8 et les fins de ligne sont celles de Windows (CRLF)
---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+/***************************************
+ * JavaScript pour l'ensemble des pages
+ **************************************/
+
+function deconnexion() {
+    // à coder
+    alert("Déconnexion en cours...");
+}
+
+function afficherNom() {
+    // à coder
+    document.getElementById("bienvenue").innerHTML = "Sheldon Cooper";
+}

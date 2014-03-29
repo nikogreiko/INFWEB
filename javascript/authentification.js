7dee2d1fea5b7c51c9266f9f61a7d621f9fe507e
nikogreiko <programmingpapas@gmail.com>
2014-03-29 13:12:39 -0400

    Commencer a mondifié les pages et js

--- javascript/authentification.js
+++ javascript/authentification.js
@@ -1,4 +1,29 @@
-/*********************************************
- * JavaScript pour la page d'authentification
- ********************************************/
+/*==========================================================================
+  JavaScript pour la page d'authentification
+ ===========================================================================*/
 
+var prenom;
+var nom;
+var courriel;
+var mdp;
+var confMdp;
+
+
+function getInscriptionInfo () {
+  prenom = document.inscription.first.value;
+  nom = document.inscription.last.value;
+  courriel = document.inscription.email.value;
+  mdp = document.inscription.signpass.value;
+  confMdp = document.inscription.confirmpass.value;
+
+	validationInfo();
+}
+
+function validationInfo () {
+  //Validation si les champs sont tous remplies.
+  if (prenom == "" || nom == "" || courriel == "" || mdp == "" || confMdp == ""){
+  	alert("Tous les champs doivent être saisies.");
+  	return false;
+  }
+  
+}
